f0159f04c9aa399bea0771916a358a66f1d336cd
Your name <Your email>
2024-03-29 19:20:54 +0100

    vercel fetchs

--- components/Header.js
+++ components/Header.js
@@ -42,7 +42,7 @@ function Header() {
   };
 
   const handleConnection = () => {
-    fetch('http://localhost:3000/users/signin', {
+    fetch('https://morning-news-backend-beryl.vercel.app/users/signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signInUsername, password: signInPassword }),
